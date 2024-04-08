bc354612465fce21dc28a2088a28c28501def6e3
dZev1 <diegozevallos04@gmail.com>
2024-04-08 18:13:09 -0300

    Hecho todo el ej 1

--- Ejercicio1.hs
+++ Ejercicio1.hs
@@ -1,3 +1,21 @@
+--1.A
 f :: Integer -> Integer
 
-f n | 
+f 1 = 8
+f 4 = 131
+f 16 = 16
+
+--1.B
+g :: Integer -> Integer
+
+g 8 = 16
+g 16 = 4
+g 131 = 1
+
+--1.C
+h :: Integer -> Integer
+k :: Integer -> Integer
+
+h n = f (g n)
+
+k n = g (f n)
--- test.hs
+++ test.hs
@@ -1,2 +1,3 @@
 doubleMe :: Integer -> Integer
 doubleMe x = x + x
+--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
