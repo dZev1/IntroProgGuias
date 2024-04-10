69794ce2d819bbe08d9402d1a430477641a7064e
dZev1 <diegozevallos04@gmail.com>
2024-04-10 18:29:44 -0300

    Cambios en algunas cosas :)

--- Practica3/Ejercicio8.hs
+++ Practica3/Ejercicio8.hs
@@ -1,7 +1,13 @@
-comparar :: Int -> Int -> Int
+comparar :: Integer -> Integer -> Integer
 comparar a b | sumaUltimosDosDigitos a < sumaUltimosDosDigitos b = 1
              | sumaUltimosDosDigitos b < sumaUltimosDosDigitos a = -1
              | sumaUltimosDosDigitos a == sumaUltimosDosDigitos b = 0
 
-sumaUltimosDosDigitos :: Int -> Int
-sumaUltimosDosDigitos x = mod (abs x) 10 + div (mod (abs x) 100 - mod (abs x) 10) 10
+digitoUnidades :: Integer -> Integer
+digitoUnidades n = mod (abs n) 10
+
+digitoDecenas :: Integer -> Integer
+digitoDecenas n = digitoUnidades (div (abs n) 10)
+
+sumaUltimosDosDigitos :: Integer -> Integer
+sumaUltimosDosDigitos x = digitoUnidades x + digitoDecenas x
