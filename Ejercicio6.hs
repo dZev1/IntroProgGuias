9333c3c0c83c6d91a4c721e67e423270f462058d
dZev1 <77645025+dZev1@users.noreply.github.com>
2024-04-10 10:32:48 -0300

    Ejercicios 1-8 movidos a la carpeta Practica3

--- Ejercicio6.hs
+++
@@ -1,4 +0,0 @@
--- Programar una funcion bisiesto :: Integer ->Bool 
-bisiesto :: Int -> Bool
-bisiesto yr | mod yr 4 == 0 && (mod yr 100 /= 0 || mod yr 400 == 0) = True
-            | otherwise = False
