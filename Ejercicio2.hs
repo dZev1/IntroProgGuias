53e0db1c1f52bfee2fc3ae96d0eafad7c041cee1
dZev1 <77645025+dZev1@users.noreply.github.com>
2024-04-10 10:24:52 -0300

    HelloThere2

--- Ejercicio2.hs
+++ Ejercicio2.hs
@@ -1,10 +1,45 @@
---2. C
+-- Especificar e implementar las siguientes funciones, incluyendo su signatura:
+
+-- absoluto: calcula el valor absoluto de un n´umero entero
+absoluto :: Integer -> Integer
+absoluto n | n >= 0 = n
+    | n < 0 = -n
+
+-- maximoabsoluto: devuelve el maximo entre el valor absoluto de dos numeros enteros
+maximoAbsoluto :: Integer -> Integer -> Integer
+maximoAbsoluto n m | absoluto n >= absoluto m = absoluto n
+                   | absoluto m >= absoluto n = absoluto m
+
+-- maximo3: devuelve el maximo entre tres numeros enteros.
 maximo3 :: Integer -> Integer -> Integer -> Integer
 maximo3 x y z | x >= y && x >= z = x
               | y >= x && y >= z = x
               | otherwise = z
 
---2. G
+-- algunoEs0: dados dos numeros racionales, decide si alguno de los dos es igual a 0
+algunoEs0 :: Float -> Float -> Bool
+-- Pattern Matching
+-- algunoEs0 x 0 = True
+-- algunoEs0 0 x = True
+
+algunoEs0 x y = x == 0 || y == 0
+
+-- ambosSon0: dados dos numeros racionales, decide si ambos son iguales a 0
+ambosSon0 :: Float -> Float -> Bool
+-- Pattern Matching
+-- ambosSon0 0 0 = True
+
+ambosSon0 x y = x == 0 && y == 0
+
+-- mismoIntervalo: dados dos numeros reales, indica si estan relacionados considerando la relacion de equivalencia en R
+-- cuyas clases de equivalencia son: (−∞, 3],(3, 7] y (7, ∞), o dicho de otra forma, si pertenecen al mismo intervalo.
+mismoIntervalo :: Float -> Float -> Bool
+mismoIntervalo x y | x <= 3 && y <= 3 = True
+                   | x <= 7 && x > 3 && y <= 7 && y > 3 = True
+                   | x > 7 && y > 7 = True
+                   | otherwise = False
+
+-- sumaDistintos: que dados tres numeros enteros calcule la suma sin sumar repetidos (si los hubiera).
 sumaDistintos :: Integer -> Integer -> Integer -> Integer
 sumaDistintos x y z | x /= y && x /= z && y /= z = x + y + z
                     | x == y && x /= z = z
@@ -12,14 +47,17 @@ sumaDistintos x y z | x /= y && x /= z && y /= z = x + y + z
                     | y == z && y /= x = x
                     | otherwise = 0
 
---2. I
+-- esMultiploDe: dados dos numeros naturales, decidir si el primero es multiplo del segundo
+esMultiploDe :: Integer -> Integer -> Bool
+esMultiploDe n m | mod n m == 0 = True
+                 | mod m n /= 0 = False
+                 | otherwise = False
 
+-- digitoUnidades: dado un numero entero, extrae su dıgito de las unidades.
 digitoUnidades :: Integer -> Integer
-
 digitoUnidades n = mod (abs n) 10
 
+-- digitoDecenas: dado un n´umero entero, extrae su d´ıgito de las decenas.
 digitoDecenas :: Integer -> Integer
-
--- 1era forma: digitoDecenas n = div (mod (abs n) 100 - mod (abs n) 10) 10
--- 2da forma:
 digitoDecenas n = digitoUnidades (div (abs n) 10)
+-- otra forma: digitoDecenas n = div (mod (abs n) 100 - mod (abs n) 10) 10
