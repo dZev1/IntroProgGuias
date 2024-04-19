93a8fe732fd9f9f3733fe857b7fcde38ff2e8274
dZev1 <diegozevallos04@gmail.com>
2024-04-19 11:51:57 -0300

    Ejercicios hechos!

--- Practica4/Ejercicio14.hs
+++ Practica4/Ejercicio14.hs
@@ -1,6 +1,6 @@
 -- 14
 sumaPotencias :: Integer -> Integer -> Integer -> Integer
-sumaPotencias q n m = (potencias q n) * (potencias q m)
+sumaPotencias q n m = potencias q n * potencias q m
 
 potencias :: Integer -> Integer -> Integer
 potencias q 1 = q
@@ -8,7 +8,7 @@ potencias q n = potencias q (n - 1) * (q ^ n)
 
 -- 16 a
 menorDivisor :: Integer -> Integer
-menorDivisor n = menorDivisorDesde 2 n 
+menorDivisor = menorDivisorDesde 2
 
 menorDivisorDesde :: Integer -> Integer -> Integer
 menorDivisorDesde m n | mod n m == 0 = m
@@ -17,3 +17,20 @@ menorDivisorDesde m n | mod n m == 0 = m
 -- 16 b
 esPrimo :: Integer -> Bool
 esPrimo n = menorDivisor n == n
+
+-- 16 c
+sonCoprimos :: Integer -> Integer -> Bool
+sonCoprimos n m = maximoComunDivisor n m == 1
+
+maximoComunDivisor :: Integer -> Integer -> Integer
+maximoComunDivisor n 0 = n
+maximoComunDivisor n m = maximoComunDivisor m (mod n m)
+
+-- 16 d
+nEsimoPrimo :: Integer -> Integer
+nEsimoPrimo 1 = 2
+nEsimoPrimo n = siguientePrimo (nEsimoPrimo (n - 1))
+
+siguientePrimo :: Integer -> Integer
+siguientePrimo n | esPrimo (n + 1) = n + 1
+                 | otherwise = siguientePrimo (n + 1)
